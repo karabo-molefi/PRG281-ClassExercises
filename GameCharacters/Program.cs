936db7699fbe29f6ac123a7965465a3e9786b1c0
Karabo Molefi <karabomolefi@icloud.com>
2026-08-11 22:17:37 +0200

    add comments to main program

---
+++ GameCharacters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a new character
+
+            // create character list
+
+            // display the character's information
+        }
+    }
+}
